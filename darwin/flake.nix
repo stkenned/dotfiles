8d4e82052759f1f630836a37ebdb1f67235e4d27
Scott Kennedy <stkenned@gmail.com>
2024-05-13 12:56:38 -0700

    moved files around

---
+++ darwin/flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Darwin system flake, copy to ~/.config/nix-darwin/flake.nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          pkgs.git
+          pkgs.vim
+        ];
+
+      # With rosetta installed, can run Intel binaries
+      nix.extraOptions = ''
+        extra-platforms = x86_64-darwin aarch64-darwin
+      '';
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # sudo with touch ID
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#MacBook
+    darwinConfigurations."MacBook" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."MacBook".pkgs;
+  };
+}
---
+++ home-darwin.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }: {
+  programs.home-manager.enable = true;
+  programs.zsh.enable = true;
+
+  imports = [
+    ./home/git.nix
+    ./home/tmux.nix
+    ./home/zsh.nix
+  ];
+
+  home.packages = [
+    pkgs.git
+    pkgs.nvim
+    pkgs.tmux
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+}
