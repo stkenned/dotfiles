f3bf6367cacd45bbb7ce075e8eb4099acaee8fd9
Scott Kennedy <stkenned@gmail.com>
2024-05-13 13:34:35 -0700

    attempt at separate config file

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Darwin system flake, copy to ~/.config/nix-darwin/flake.nix";
+  description = "Darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -8,56 +8,13 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.git
-          pkgs.vim
-        ];
-
-      # With rosetta installed, can run Intel binaries
-      nix.extraOptions = ''
-        extra-platforms = x86_64-darwin aarch64-darwin
-      '';
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        screencapture.location = "~/Screenshots";
-      };
-
-      # sudo with touch ID
-      security.pam.enableSudoTouchIdAuth = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#MacBook
     darwinConfigurations."MacBook" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        ./configuration.nix
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
