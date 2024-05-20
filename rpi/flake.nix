3331daf66e31f0c6a1592f038bcaafba4b43dd95
Scott Kennedy <scott@repl.it>
2024-05-20 02:46:20 +0000

    Fix formatting

--- rpi/flake.nix
+++ rpi/flake.nix
@@ -15,13 +15,13 @@
       system = "aarch64-linux";
       modules = [
         ./configuration.nix
-	home-manager.nixosModules.home-manager
-	{
+	      home-manager.nixosModules.home-manager
+	      {
           home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.backupFileExtension = "backup";
-	  home-manager.users.scott = import ../home.nix;
-	}
+	        home-manager.useUserPackages = true;
+	        home-manager.backupFileExtension = "backup";
+	        home-manager.users.scott = import ../home.nix;
+	      }
       ];
     };
 
