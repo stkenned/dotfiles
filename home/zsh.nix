c6569a3d607e9acd4478ca5074c3db89b955ee06
Scott Kennedy <stkenned@gmail.com>
2026-01-20 15:33:42 -0800

    fix another warning

--- home/zsh.nix
+++ home/zsh.nix
@@ -14,7 +14,7 @@
       ];
     };
   
-    initExtra = ''
+    initContent = ''
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
